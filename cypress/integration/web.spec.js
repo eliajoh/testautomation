a16a6b4c30d6596587519bbbf76743baf5995b5d
eliajoh <48404721+eliajoh@users.noreply.github.com>
2022-02-18 13:31:02 +0100

    Added second basic test

--- cypress/integration/web.spec.js
+++ cypress/integration/web.spec.js
@@ -1,3 +1,5 @@
+  
+  //Driver-test
   describe('Navigate from home page to driver page', () => {
     it('clicks the link "drivers"', () => {
       cy.visit('https://formula1stats.herokuapp.com/')
@@ -14,4 +16,23 @@
     })
   })
 
-  
+//Constructor-test
+  describe('Navigate from home page to Constructors page', () => {
+    it('clicks the link "Constructors"', () => {
+      cy.visit('https://formula1stats.herokuapp.com/constructors')
+      
+      cy.get('#constructor-year-column').click().contains('2016').click()
+
+      cy.get('#constructor-year-column').click().contains('2018').click()
+
+      cy.get('#menu-items-container').contains('Home').click()
+
+      cy.get('#menu-items-container').contains('Constructors').click()
+
+      cy.get('#constructor-year-column').click().contains('2015').click()
+
+
+        //byt år ett par gånger, gå till annan sida, gå dit igen och byt år igen
+
+    })
+  })
