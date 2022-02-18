574000fb1588f56d140187729ab9d7506ffdf309
eliajoh <48404721+eliajoh@users.noreply.github.com>
2022-02-18 13:01:41 +0100

    Updated first test

--- cypress/integration/web.spec.js
+++ cypress/integration/web.spec.js
@@ -3,5 +3,15 @@
       cy.visit('https://formula1stats.herokuapp.com/')
       
       cy.get('#menu-items-container').contains('Drivers').click()
+
+      cy.get('#driver-column').click().contains('Mika Häkkinen').click()
+
+      cy.get('#menu-items-container').contains('Home').click()
+
+      //välj en annan förare, gå tillbaka till start-sidan, se om ny förare väljs eller det är den gamla
+      
+
     })
   })
+
+  
